9ff0e70a150ad18e44b51e4e10573ea723f60b57
Dummyc0m <m.steve.jobs@me.com>
2016-11-19 14:53:50 -0500

    copies the file instead of screwing it over

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
     Author Tobias Koppers @sokra
     Edited Guilherme Bernal @lbguilherme
 */
+var loaderUtils = require("loader-utils");
+var fs = require('fs');
+
 module.exports = function(content) {
     this.cacheable && this.cacheable();
     if (!this.emitFile) throw new Error("emitFile is required from module system");
@@ -12,7 +15,33 @@ module.exports = function(content) {
         content: content,
         regExp: query.regExp
     });
-    this.emitFile(url, content);
+    copyFile(this.resourcePath, url, function () {
+        console.log('File copied from ' + this.resourcePath + ' to ' + url)
+    })
     return "try { global.process.dlopen(module, __webpack_public_path__ + " + JSON.stringify(url) + "); } catch(e) { " +
         "throw new Error('Cannot load native module ' + " + JSON.stringify(url) + " + ': ' + e); }";
+}
+
+var copyFile = function (source, target, cb) {
+  var cbCalled = false;
+
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
 }
